9f405622140451f4f1c879a1c393171c5ffbeeb3
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-09-14 12:34:35 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,6 +3,7 @@
 
 import os
 
+LINK = "T.ME/MOTECH_yt"
 
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
@@ -13,4 +14,4 @@ class Config(object):
 
     # Get this api from https://www.remove.bg/b/background-removal-api
     RemoveBG_API = os.environ.get("RemoveBG_API", "")
-    LINK = "T.ME/MOTECH_yt"
+    
